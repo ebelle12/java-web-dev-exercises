c8a77054718879c2a73c27597e092699b41e4053
evabelle <stl_graduate@yahoo.com>
2021-08-27 23:55:22 -0500

    Studio: Area of Circle

---
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,21 @@
+package org.launchcode.java.studios.areaofacircle;
+
+
+// create method called getArea
+//take in double radius
+// return area
+
+
+public class Circle {
+    // declare variables
+
+    // create constructor
+    public Circle() {
+
+    }
+    // create method
+    public static Double getArea(Double radius) {
+        return 3.14 * radius * radius;
+    }
+
+}
